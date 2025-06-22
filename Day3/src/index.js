c93fb8bb56c1a4f8c97c378c4b85540306e37d84
kamal126 <kamalchandra265@gmail.com>
2025-06-22 13:08:40 +0530

    utils code handle

--- Day3/src/index.js
+++ Day3/src/index.js
@@ -1,18 +1,35 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from './db/index.js';
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
+
 // directly load environment variable so changes in package.json
 /**
  * "script" :
  *  "dev": "nodemon src/index.js"       // before 
  *  "dev": "nodemon -r dotenv/config --exprimental-json.modules src/index.js"   // after
  */
-connectDB()
 
+PORT = process.env.PORT
+connectDB()
+.then(
+    app.on("error", () => {
+        console.log("ERROR: ", error)
+        throw error
+    }),
+
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port: ${PORT}`)
+        
+    })
+)
+.catch((error)=>{
+    console.log("MONGODB conncetion failed !!! ", error);
+})
 
 
 
